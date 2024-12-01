6d7881af7f6b2565cf62f245545ab1f0a9fbb1c6
EminSgl <eminakin@hotmail.de>
2024-12-01 22:50:17 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./LandingPage";
+import ProductListing from "./ProductListing";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductListing />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
