234393435c01a95e44b43fa546b36f9ae1a90f05
EminSgl <eminakin@hotmail.de>
2024-12-01 22:54:08 +0100

    Create ProductsPage.js

---
+++ src/ProductsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1>Product Listing Page</h1>
+      <p>Here will be the list of houseplants!</p>
+    </div>
+  );
+};
+
+export default ProductsPage;
