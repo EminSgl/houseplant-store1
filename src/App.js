f07fffdede235ffa7db4e621dca1a2985ee10087
EminSgl <eminakin@hotmail.de>
2024-12-06 15:02:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage";
-import ProductListing from "./ProductListing";
+import ProductListing from './ProductListing';
 
 function App() {
   return (
