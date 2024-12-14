bcbbbf6bd3ac6ee28c81573ea36d0e45a8d9b762
EminSgl <eminakin@hotmail.de>
2024-12-14 17:29:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import ProductListing from "./ProductListing";
+import ShoppingCart from "./ShoppingCart";
 import "./App.css"; // Falls du CSS für den Header hinzufügen möchtest
 
 function App() {
+  // Zustand für den Warenkorb
+  const [cart, setCart] = useState([]);
+
   return (
     <Router>
       <div>
@@ -18,6 +22,9 @@ function App() {
               <li>
                 <Link to="/products">Products</Link>
               </li>
+              <li>
+                <Link to="/cart">Cart ({cart.reduce((acc, item) => acc + item.quantity, 0)})</Link>
+              </li>
             </ul>
           </nav>
         </header>
@@ -25,7 +32,14 @@ function App() {
         {/* Routen */}
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/products" element={<ProductListing />} />
+          <Route
+            path="/products"
+            element={<ProductListing cart={cart} setCart={setCart} />}
+          />
+          <Route
+            path="/cart"
+            element={<ShoppingCart cart={cart} setCart={setCart} />}
+          />
         </Routes>
       </div>
     </Router>
