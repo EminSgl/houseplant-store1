8f12e4f6e98ef8231fe5ddb7959890b474a629c4
EminSgl <eminakin@hotmail.de>
2024-12-14 17:22:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import LandingPage from "./LandingPage";
-import ProductListing from './ProductListing';
+import ProductListing from "./ProductListing";
+import "./App.css"; // Falls du CSS für den Header hinzufügen möchtest
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/products" element={<ProductListing />} />
-      </Routes>
+      <div>
+        {/* Header mit Navigation */}
+        <header className="app-header">
+          <nav>
+            <ul className="nav-links">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/products">Products</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        {/* Routen */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/products" element={<ProductListing />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
