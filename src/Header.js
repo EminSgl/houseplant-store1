fd3c9220b9693e9b81db1a721d00fd89a6624342
EminSgl <eminakin@hotmail.de>
2024-12-14 17:27:15 +0100

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = ({ cartCount }) => {
+const Header = ({ cart }) => {
+  const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
     <header>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
-        <Link to="/cart">Cart ({cartCount})</Link>
+        <Link to="/cart">Cart ({totalItems})</Link>
       </nav>
     </header>
   );
